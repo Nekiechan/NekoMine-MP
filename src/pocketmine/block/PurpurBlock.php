54717b39e6f9dfc3415a1856ccdb1265ad568e98
Nekiechan <nekiechan678@gmail.com>
2017-06-22 11:35:50 -0500

    New Blocks

---
+++ src/pocketmine/block/PurpurBlock.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ *
+ *  ____          
+ * |  __|_              _
+ * | |__| |      _    _(_)_ __   ___
+ * |  __| |_   _| |  | | | '_ \ / _ \
+ * | |__| | | | | |/\| | | | | | (_) |
+ * |____|_|\ \/ \__/\__/_|_| |_|\___ |
+ *         _|  /                 __| |
+ *        |___/                 |____/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author FunnyBuddys
+ * @link http://www.github.net/H4PM
+ * 
+ *
+*/
+
+namespace pocketmine\block;
+
+use pocketmine\item\Item;
+use pocketmine\item\Tool;
+use pocketmine\Player;
+
+class PurpurBlock extends Solid{
+
+	protected $id = self::PURPUR;
+
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
+
+	public function getName(){
+		static $names = [
+			0 => "Purpur Block",
+			1 => "Unknown",
+			2 => "Purpur Pillar",
+		];
+		return $names[$this->meta & 0x0f]??"Unknown";
+	}
+
+    public function getHardness(){
+        return 1.5;
+    }
+
+    public function getResistance(){
+        return 30;
+    }
+	
+	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
+		if($this->meta === 2){
+			$faces = [
+				0 => 0,
+				1 => 0,
+				2 => 0b1000,
+				3 => 0b1000,
+				4 => 0b0100,
+				5 => 0b0100,
+			];
+			$this->meta = ($this->meta & 0x03) | $faces[$face];
+		}
+		$this->getLevel()->setBlock($block, $this, true, true);
+		return true;
+	}
+
+	public function getToolType(){
+		return Tool::TYPE_PICKAXE;
+	}
+
+    public function getDrops(Item $item){
+        if ($item->isPickaxe() >= Tool::TIER_WOODEN) {
+            return [
+                [$this->id, $this->meta & 0x0f, 1]
+            ];
+        } else {
+            return [];
+        }
+    }
+}
