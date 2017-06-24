956833e4d992395523206ea84c4b545e5d7d85cd
Nekiechan <nekiechan678@gmail.com>
2017-06-24 15:48:10 -0500

    New command!

---
+++ src/pocketmine/command/defaults/iam.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pocketmine\command\defaults;
+
+use pocketmine\command\CommandSender;
+use pocketmine\event\TranslationContainer;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class IamCommand extends VanillaCommand{
+
+	public function __construct($name){
+		parent::__construct(
+			$name,
+      "Action Command",
+			"/iam <action>"
+		);
+		$this->setPermission("pocketmine.command.iam");
+	}
+
+	public function execute(CommandSender $sender, $currentAlias, array $args){
+		if(!$this->testPermission($sender)){
+			return true;
+		}
+
+		if(count($args) === 0){
+      $sender->sendMessage("§l§cPut a action! §a/iam <action>");
+			return false;
+		}
+    $this->getServer()->broadcastMessage("§a×§c" . $sender->getName() . " §a" . implode(" ", $args)  . "!×");
+
+		
+		return true;
+	}
+}
