635fdeb99916c976ac6ea88369b21b3c54a2bb15
Nekiechan <nekiechan678@gmail.com>
2017-07-07 13:36:23 -0500

    Update to 1.2.0Beta

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -73,7 +73,7 @@ namespace pocketmine {
 	use pocketmine\wizard\SetupWizard;
 	use raklib\RakLib;
 
-	const VERSION = "1.1.0Beta(32Bit)";
+	const VERSION = "1.2.0Beta(32Bit)";
 	const API_VERSION = "2.0.0";
 	const CODENAME = "§l§aNeko§r";
 
