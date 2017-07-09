65574d9a4663900d8cde4ebc1f0e839224931a0c
Nekiechan <nekiechan678@gmail.com>
2017-07-08 20:40:11 -0500

    added listops command

--- src/pocketmine/command/defaults/ListOpCommand.php
+++ src/pocketmine/command/defaults/ListOpCommand.php
@@ -1,4 +1,25 @@
 <?php
-public function getOps(){
-		return $this->operators;
+declare(strict_types=1);
+namespace pocketmine\command\defaults;
+use pocketmine\command\CommandSender;
+use pocketmine\event\TranslationContainer;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+class ListOpCommand extends VanillaCommand{
+	public function __construct($name){
+		parent::__construct(
+			$name,
+      "Lists server Operators",
+			"/listop"
+		);
+		$this->setPermission("pocketmine.command.listop");
 	}
+	public function execute(CommandSender $sender, $currentAlias, array $args){
+		if(!$this->testPermission($sender)){
+			return true;
+		}
+   $sender->sendMessage("- - - Server Ops - - -");
+   $sender->sendMessage($sender->getServer()->getOps());
+		return true;
+	}
+}
