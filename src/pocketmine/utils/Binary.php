df7fd58e066fe866caefb03a20dfc8790d500683
Nekiechan <nekiechan678@gmail.com>
2017-07-12 11:38:50 -0500

    fixed players unable to join on 64Bit systems

--- src/pocketmine/utils/Binary.php
+++ src/pocketmine/utils/Binary.php
@@ -647,7 +647,8 @@ class Binary{
 	 */
 	public static function writeVarLong($v) : string{
 		if(PHP_INT_SIZE === 8){
-			return self::writeVarLong_64($v);
+			//return self::writeVarLong_64($v); Force 32-bit on 64bit so it wont block players from joining on 64Bit systems
+			return self::writeVarLong_32((string) $v);
 		}else{
 			return self::writeVarLong_32((string) $v);
 		}
