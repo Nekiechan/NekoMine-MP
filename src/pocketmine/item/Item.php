1fe7a15a0614b8056933a4e31e65dca0fd6582b6
Nekiechan <nekiechan678@gmail.com>
2017-07-13 13:58:13 -0500

    organized everything

--- src/pocketmine/item/Item.php
+++ src/pocketmine/item/Item.php
@@ -108,174 +108,204 @@ class Item implements ItemIds, \JsonSerializable {
 	 */
 	public static function init($readFromJson = false){
 		if(self::$list === null){
-			//TODO: Sort this mess into some kind of order
 			self::$list = new \SplFixedArray(65536);
-			self::$list[self::SUGARCANE] = Sugarcane::class;
-			self::$list[self::ENDER_PEARL] = EnderPearl::class;
-			self::$list[self::EYE_OF_ENDER] = EyeOfEnder::class;
-			self::$list[self::DRAGONS_BREATH] = DragonsBreath::class;
-			self::$list[self::SHULKER_SHELL] = ShulkerShell::class;
-			self::$list[self::POPPED_CHORUS_FRUIT] = PoppedChorusFruit::class;
-			self::$list[self::WHEAT_SEEDS] = WheatSeeds::class;
-			self::$list[self::PUMPKIN_SEEDS] = PumpkinSeeds::class;
-			self::$list[self::MELON_SEEDS] = MelonSeeds::class;
-			self::$list[self::MUSHROOM_STEW] = MushroomStew::class;
-			self::$list[self::RABBIT_STEW] = RabbitStew::class;
+			//A
+			self::$list[self::ACACIA_DOOR] = AcaciaDoor::class;
+			self::$list[self::APPLE] = Apple::class;
+			self::$list[self::ARROW] = Arrow::class;
+			//B
 			self::$list[self::BEETROOT_SOUP] = BeetrootSoup::class;
 			self::$list[self::BEETROOT_SEEDS] = BeetrootSeeds::class;
-			self::$list[self::SIGN] = Sign::class;
-			self::$list[self::WOODEN_DOOR] = WoodenDoor::class;
-			self::$list[self::SPRUCE_DOOR] = SpruceDoor::class;
-			self::$list[self::BIRCH_DOOR] = BirchDoor::class;
-			self::$list[self::JUNGLE_DOOR] = JungleDoor::class;
-			self::$list[self::ACACIA_DOOR] = AcaciaDoor::class;
-			self::$list[self::DARK_OAK_DOOR] = DarkOakDoor::class;
 			self::$list[self::BUCKET] = Bucket::class;
-			self::$list[self::IRON_DOOR] = IronDoor::class;
-			self::$list[self::CAKE] = Cake::class;
-			self::$list[self::BED] = Bed::class;
-			self::$list[self::PAINTING] = Painting::class;
-			self::$list[self::COAL] = Coal::class;
-			self::$list[self::APPLE] = Apple::class;
-			self::$list[self::SPAWN_EGG] = SpawnEgg::class;
-			self::$list[self::DIAMOND] = Diamond::class;
-			self::$list[self::STICK] = Stick::class;
-			self::$list[self::SNOWBALL] = Snowball::class;
 			self::$list[self::BOWL] = Bowl::class;
-			self::$list[self::FEATHER] = Feather::class;
+			self::$list[self::BIRCH_DOOR] = BirchDoor::class;
+			self::$list[self::BED] = Bed::class;
 			self::$list[self::BRICK] = Brick::class;
-			self::$list[self::LEATHER_CAP] = LeatherCap::class;
-			self::$list[self::LEATHER_TUNIC] = LeatherTunic::class;
-			self::$list[self::LEATHER_PANTS] = LeatherPants::class;
-			self::$list[self::LEATHER_BOOTS] = LeatherBoots::class;
+			self::$list[self::BOW] = Bow::class;
+			self::$list[self::BOAT] = Boat::class;
+			self::$list[self::BREAD] = Bread::class;
+			self::$list[self::BOOK] = Book::class;
+			self::$list[self::BONE] = Bone::class;
+			self::$list[self::BAKED_POTATO] = BakedPotato::class;
+			self::$list[self::BREWING_STAND] = BrewingStand::class;
+			self::$list[self::BEETROOT] = Beetroot::class;
+			self::$list[self::BLAZE_POWDER] = BlazePowder::class;
+			//C
+			self::$list[self::CAKE] = Cake::class;
+			self::$list[self::COAL] = Coal::class;
 			self::$list[self::CHAIN_HELMET] = ChainHelmet::class;
 			self::$list[self::CHAIN_CHESTPLATE] = ChainChestplate::class;
 			self::$list[self::CHAIN_LEGGINGS] = ChainLeggings::class;
 			self::$list[self::CHAIN_BOOTS] = ChainBoots::class;
-			self::$list[self::IRON_HELMET] = IronHelmet::class;
-			self::$list[self::IRON_CHESTPLATE] = IronChestplate::class;
-			self::$list[self::IRON_LEGGINGS] = IronLeggings::class;
-			self::$list[self::IRON_BOOTS] = IronBoots::class;
-			self::$list[self::GOLD_HELMET] = GoldHelmet::class;
-			self::$list[self::GOLD_CHESTPLATE] = GoldChestplate::class;
-			self::$list[self::GOLD_LEGGINGS] = GoldLeggings::class;
-			self::$list[self::GOLD_BOOTS] = GoldBoots::class;
+			self::$list[self::COOKED_FISH] = CookedFish::class;
+			self::$list[self::COOKED_PORKCHOP] = CookedPorkchop::class;
+			self::$list[self::CLAY] = Clay::class;
+			self::$list[self::COMPASS] = Compass::class;
+			self::$list[self::CLOCK] = Clock::class;
+			self::$list[self::COOKIE] = Cookie::class;
+			self::$list[self::COOKED_CHICKEN] = CookedChicken::class;
+			self::$list[self::CARROT] = Carrot::class;
+			self::$list[self::CAMERA] = Camera::class; //Man these things were cool and interesting back in the day, jesh Mojang!
+			self::$list[self::COOKED_RABBIT] = CookedRabbit::class;
+			self::$list[self::CAULDRON] = Cauldron::class;
+			self::$list[self::COOKED_MUTTON] = CookedMutton::class;
+			self::$list[self::CHORUS_FRUIT] = ChorusFruit::class;
+			//D
+			self::$list[self::DRAGONS_BREATH] = DragonsBreath::class;
+			self::$list[self::DARK_OAK_DOOR] = DarkOakDoor::class;
+			self::$list[self::DIAMOND] = Diamond::class;
 			self::$list[self::DIAMOND_HELMET] = DiamondHelmet::class;
 			self::$list[self::DIAMOND_CHESTPLATE] = DiamondChestplate::class;
 			self::$list[self::DIAMOND_LEGGINGS] = DiamondLeggings::class;
 			self::$list[self::DIAMOND_BOOTS] = DiamondBoots::class;
-			self::$list[self::IRON_SWORD] = IronSword::class;
-			self::$list[self::IRON_INGOT] = IronIngot::class;
-			self::$list[self::GOLD_INGOT] = GoldIngot::class;
-			self::$list[self::IRON_SHOVEL] = IronShovel::class;
-			self::$list[self::IRON_PICKAXE] = IronPickaxe::class;
-			self::$list[self::IRON_AXE] = IronAxe::class;
-			self::$list[self::IRON_HOE] = IronHoe::class;
 			self::$list[self::DIAMOND_SWORD] = DiamondSword::class;
 			self::$list[self::DIAMOND_SHOVEL] = DiamondShovel::class;
 			self::$list[self::DIAMOND_PICKAXE] = DiamondPickaxe::class;
 			self::$list[self::DIAMOND_AXE] = DiamondAxe::class;
 			self::$list[self::DIAMOND_HOE] = DiamondHoe::class;
+			self::$list[self::DYE] = Dye::class;
+			//E
+			self::$list[self::ENDER_PEARL] = EnderPearl::class;
+			self::$list[self::EYE_OF_ENDER] = EyeOfEnder::class;
+			//Original
+			self::$list[self::ENCHANTED_GOLDEN_APPLE] = GoldenAppleEnchanted::class;
+			//Genisys
+			self::$list[self::ENCHANTED_GOLDEN_APPLE] = EnchantedGoldenApple::class;
+			self::$list[self::ENCHANTING_BOTTLE] = EnchantingBottle::class;
+			self::$list[self::EGG] = Egg::class;
+			self::$list[self::EMPTY_MAP] = EmptyMap::class;
+			self::$list[self::ENCHANTED_BOOK] = EnchantedBook::class;
+			self::$list[self::EMERALD] = Emerald::class;
+			self::$list[self::ELYTRA] = Elytra::class;
+			//F
+			self::$list[self::FEATHER] = Feather::class;
+			self::$list[self::FLINT_STEEL] = FlintSteel::class;
+			self::$list[self::FLINT] = Flint::class;
+			self::$list[self::FILLED_MAP] = FilledMap::class;
+			self::$list[self::FLOWER_POT] = FlowerPot::class;
+			self::$list[self::FERMENTED_SPIDER_EYE] = FermentedSpiderEye::class;
+			self::$list[self::FIRE_CHARGE] = FireCharge::class;
+			//G
+			self::$list[self::GOLD_HELMET] = GoldHelmet::class;
+			self::$list[self::GOLD_CHESTPLATE] = GoldChestplate::class;
+			self::$list[self::GOLD_LEGGINGS] = GoldLeggings::class;
+			self::$list[self::GOLD_BOOTS] = GoldBoots::class;
 			self::$list[self::GOLD_SWORD] = GoldSword::class;
 			self::$list[self::GOLD_SHOVEL] = GoldShovel::class;
 			self::$list[self::GOLD_PICKAXE] = GoldPickaxe::class;
 			self::$list[self::GOLD_AXE] = GoldAxe::class;
 			self::$list[self::GOLD_HOE] = GoldHoe::class;
+			self::$list[self::GOLD_INGOT] = GoldIngot::class;
+			self::$list[self::GLASS_BOTTLE] = GlassBottle::class;
+			self::$list[self::GUNPOWDER] = Gunpowder::class;
+			self::$list[self::GOLDEN_APPLE] = GoldenApple::class;
+			self::$list[self::GLOWSTONE_DUST] = GlowstoneDust::class;
+			self::$list[self::GOLD_NUGGET] = GoldNugget::class;
+			self::$list[self::GOLDEN_CARROT] = GoldenCarrot::class;
+			self::$list[self::GLISTERING_MELON] = GlisteringMelon::class;
+			//H
+			self::$list[self::HOPPER] = Hopper::class;
+			//I
+			self::$list[self::IRON_HELMET] = IronHelmet::class;
+			self::$list[self::IRON_CHESTPLATE] = IronChestplate::class;
+			self::$list[self::IRON_LEGGINGS] = IronLeggings::class;
+			self::$list[self::IRON_BOOTS] = IronBoots::class;
+			self::$list[self::IRON_SWORD] = IronSword::class;
+			self::$list[self::IRON_INGOT] = IronIngot::class;
+			self::$list[self::IRON_DOOR] = IronDoor::class;
+			self::$list[self::IRON_SHOVEL] = IronShovel::class;
+			self::$list[self::IRON_PICKAXE] = IronPickaxe::class;
+			self::$list[self::IRON_AXE] = IronAxe::class;
+			self::$list[self::IRON_HOE] = IronHoe::class;
+			self::$list[self::ITEM_FRAME] = ItemFrame::class;
+			//J
+			self::$list[self::JUNGLE_DOOR] = JungleDoor::class;
+			//K
+			
+			//L
+			self::$list[self::LEATHER_CAP] = LeatherCap::class;
+			self::$list[self::LEATHER_TUNIC] = LeatherTunic::class;
+			self::$list[self::LEATHER_PANTS] = LeatherPants::class;
+			self::$list[self::LEATHER_BOOTS] = LeatherBoots::class;
+			self::$list[self::LEATHER] = Leather::class;
+			//M
+			self::$list[self::MELON_SEEDS] = MelonSeeds::class;
+			self::$list[self::MUSHROOM_STEW] = MushroomStew::class;
+			self::$list[self::MINECART] = Minecart::class;
+			self::$list[self::MELON] = Melon::class;
+			self::$list[self::MAGMA_CREAM] = MagmaCream::class;
+			//N
+			self::$list[self::NETHER_QUARTZ] = NetherQuartz::class;
+			self::$list[self::NETHER_BRICK] = NetherBrick::class;
+			self::$list[self::NETHER_WART] = NetherWart::class;
+			self::$list[self::NETHER_STAR] = NetherStar::class;
+			//O
+			
+			//P
+			self::$list[self::PRISMARINE_CRYSTALS] = PrismarineCrystals::class;
+			self::$list[self::POPPED_CHORUS_FRUIT] = PoppedChorusFruit::class;
+			self::$list[self::PUMPKIN_SEEDS] = PumpkinSeeds::class;[self::PRISMARINE_SHARD] = PrismarineShard::class;
+			self::$list[self::PAINTING] = Painting::class;
+			self::$list[self::POTION] = Potion::class;
+			self::$list[self::PAPER] = Paper::class;
+			self::$list[self::POTATO] = Potato::class;
+			self::$list[self::PUMPKIN_PIE] = PumpkinPie::class;
+			//Q
+			self::$list[self::QUARTZ] = Quartz::class;
+			//R
+			self::$list[self::RABBIT_STEW] = RabbitStew::class;
+			self::$list[self::RAW_FISH] = Fish::class;
+			self::$list[self::RAW_PORKCHOP] = RawPorkchop::class;
+			self::$list[self::REDSTONE] = Redstone::class;
+			self::$list[self::RAW_BEEF] = RawBeef::class;
+			self::$list[self::RAW_CHICKEN] = RawChicken::class;
+			self::$list[self::RAW_RABBIT] = RawRabbit::class;
+			self::$list[self::REPEATER] = Repeater::class;
+			self::$list[self::ROTTEN_FLESH] = RottenFlesh::class;
+			self::$list[self::RAW_MUTTON] = RawMutton::class;
+			//S
+			self::$list[self::SUGARCANE] = Sugarcane::class;
+			self::$list[self::SHULKER_SHELL] = ShulkerShell::class;
+			self::$list[self::SIGN] = Sign::class;
+			self::$list[self::SPRUCE_DOOR] = SpruceDoor::class;
+			self::$list[self::SPAWN_EGG] = SpawnEgg::class;
+			self::$list[self::STICK] = Stick::class;
+			self::$list[self::SNOWBALL] = Snowball::class;
 			self::$list[self::STONE_SWORD] = StoneSword::class;
 			self::$list[self::STONE_SHOVEL] = StoneShovel::class;
 			self::$list[self::STONE_PICKAXE] = StonePickaxe::class;
 			self::$list[self::STONE_AXE] = StoneAxe::class;
 			self::$list[self::STONE_HOE] = StoneHoe::class;
+			self::$list[self::SHEARS] = Shears::class;
+			self::$list[self::SPLASH_POTION] = SplashPotion::class;
+			self::$list[self::STRING] = ItemString::class;
+			self::$list[self::SUGAR] = Sugar::class;
+			self::$list[self::SKULL] = Skull::class;
+			self::$list[self::SPIDER_EYE] = SpiderEye::class;
+			self::$list[self::STEAK] = Steak::class;
+			//T
+			
+			//U
+			
+			//V
+			
+			//W
+			self::$list[self::WHEAT_SEEDS] = WheatSeeds::class;
 			self::$list[self::WOODEN_SWORD] = WoodenSword::class;
 			self::$list[self::WOODEN_SHOVEL] = WoodenShovel::class;
 			self::$list[self::WOODEN_PICKAXE] = WoodenPickaxe::class;
 			self::$list[self::WOODEN_AXE] = WoodenAxe::class;
 			self::$list[self::WOODEN_HOE] = WoodenHoe::class;
-			self::$list[self::FLINT_STEEL] = FlintSteel::class;
-			self::$list[self::SHEARS] = Shears::class;
-			self::$list[self::BOW] = Bow::class;
-            self::$list[self::ENCHANTED_GOLDEN_APPLE] = GoldenAppleEnchanted::class;
-			self::$list[self::RAW_FISH] = Fish::class;
-			self::$list[self::COOKED_FISH] = CookedFish::class;
-
-			self::$list[self::NETHER_QUARTZ] = NetherQuartz::class;
-			self::$list[self::POTION] = Potion::class;
-			self::$list[self::GLASS_BOTTLE] = GlassBottle::class;
-			self::$list[self::SPLASH_POTION] = SplashPotion::class;
-			self::$list[self::ENCHANTING_BOTTLE] = EnchantingBottle::class;
-			self::$list[self::BOAT] = Boat::class;
-			self::$list[self::MINECART] = Minecart::class;
-
-			self::$list[self::ARROW] = Arrow::class;
-			self::$list[self::STRING] = ItemString::class;
-			self::$list[self::GUNPOWDER] = Gunpowder::class;
+			self::$list[self::WOODEN_DOOR] = WoodenDoor::class;
 			self::$list[self::WHEAT] = Wheat::class;
-			self::$list[self::BREAD] = Bread::class;
-			self::$list[self::FLINT] = Flint::class;
-			self::$list[self::FLINT] = Flint::class;
-			self::$list[self::RAW_PORKCHOP] = RawPorkchop::class;
-			self::$list[self::COOKED_PORKCHOP] = CookedPorkchop::class;
-			self::$list[self::GOLDEN_APPLE] = GoldenApple::class;
-			self::$list[self::MINECART] = Minecart::class;
-			self::$list[self::REDSTONE] = Redstone::class;
-			self::$list[self::LEATHER] = Leather::class;
-			self::$list[self::CLAY] = Clay::class;
-			self::$list[self::PAPER] = Paper::class;
-			self::$list[self::BOOK] = Book::class;
-			self::$list[self::SLIMEBALL] = Slimeball::class;
-			self::$list[self::EGG] = Egg::class;
-			self::$list[self::COMPASS] = Compass::class;
-			self::$list[self::CLOCK] = Clock::class;
-			self::$list[self::GLOWSTONE_DUST] = GlowstoneDust::class;
-			self::$list[self::DYE] = Dye::class;
-			self::$list[self::BONE] = Bone::class;
-			self::$list[self::SUGAR] = Sugar::class;
-			self::$list[self::COOKIE] = Cookie::class;
-			self::$list[self::FILLED_MAP] = FilledMap::class;
-			self::$list[self::EMPTY_MAP] = EmptyMap::class;
-			self::$list[self::MELON] = Melon::class;
-			self::$list[self::RAW_BEEF] = RawBeef::class;
-			self::$list[self::STEAK] = Steak::class;
-			self::$list[self::RAW_CHICKEN] = RawChicken::class;
-			self::$list[self::COOKED_CHICKEN] = CookedChicken::class;
-			self::$list[self::GOLD_NUGGET] = GoldNugget::class;
-			self::$list[self::EMERALD] = Emerald::class;
-			self::$list[self::ITEM_FRAME] = ItemFrame::class;
-			self::$list[self::FLOWER_POT] = FlowerPot::class;
-			self::$list[self::CARROT] = Carrot::class;
-			self::$list[self::POTATO] = Potato::class;
-			self::$list[self::BAKED_POTATO] = BakedPotato::class;
-			self::$list[self::PUMPKIN_PIE] = PumpkinPie::class;
-			self::$list[self::NETHER_BRICK] = NetherBrick::class;
-			self::$list[self::QUARTZ] = Quartz::class;
-			self::$list[self::BREWING_STAND] = BrewingStand::class;
-			self::$list[self::CAMERA] = Camera::class;
-			self::$list[self::BEETROOT] = Beetroot::class;
-			self::$list[self::SKULL] = Skull::class;
-			self::$list[self::RAW_RABBIT] = RawRabbit::class;
-			self::$list[self::COOKED_RABBIT] = CookedRabbit::class;
-			self::$list[self::GOLDEN_CARROT] = GoldenCarrot::class;
-			self::$list[self::NETHER_WART] = NetherWart::class;
-			self::$list[self::SPIDER_EYE] = SpiderEye::class;
-			self::$list[self::FERMENTED_SPIDER_EYE] = FermentedSpiderEye::class;
-			self::$list[self::BLAZE_POWDER] = BlazePowder::class;
-			self::$list[self::MAGMA_CREAM] = MagmaCream::class;
-			self::$list[self::GLISTERING_MELON] = GlisteringMelon::class;
-			self::$list[self::ENCHANTED_BOOK] = EnchantedBook::class;
-			self::$list[self::REPEATER] = Repeater::class;
-			self::$list[self::CAULDRON] = Cauldron::class;
-			self::$list[self::ROTTEN_FLESH] = RottenFlesh::class;
-			self::$list[self::ENCHANTED_GOLDEN_APPLE] = EnchantedGoldenApple::class;
-			self::$list[self::RAW_MUTTON] = RawMutton::class;
-			self::$list[self::COOKED_MUTTON] = CookedMutton::class;
-			self::$list[self::HOPPER] = Hopper::class;
-			self::$list[self::ELYTRA] = Elytra::class;
-			self::$list[self::NETHER_STAR] = NetherStar::class;
-			self::$list[self::CHORUS_FRUIT] = ChorusFruit::class;
-			self::$list[self::PRISMARINE_CRYSTALS] = PrismarineCrystals::class;
-			self::$list[self::PRISMARINE_SHARD] = PrismarineShard::class;
-			self::$list[self::FIRE_CHARGE] = FireCharge::class;
-
+			//X
+			
+			//Y
+			
+			//Z
+			
+			//Other
+			
 			for($i = 0; $i < 256; ++$i){
 				if(Block::$list[$i] !== null){
 					self::$list[$i] = Block::$list[$i];
@@ -290,7 +320,8 @@ class Item implements ItemIds, \JsonSerializable {
 
 	private static function initCreativeItems(){
 		self::clearCreativeItems();
-
+		//add blocks here .3. for custom blocks in cmode inventory
+		
 		$creativeItems = new Config(Server::getInstance()->getFilePath() . "src/pocketmine/resources/creativeitems.json", Config::JSON, []);
 
 		foreach($creativeItems->getAll() as $data){
