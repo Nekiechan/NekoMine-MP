63140c4edea3d7a4fc9fa7a1424a289dbbc10812
Nekiechan <nekiechan678@gmail.com>
2017-07-13 11:36:30 -0500

    Updated

--- src/pocketmine/block/WetSponge.php
+++ src/pocketmine/block/WetSponge.php
@@ -2,46 +2,53 @@
 
 /*
  *
- * | |  | |         | |                          
- * | |__| |_   _  __| |_ __ __ _  ___ ___  _ __  
- * |  __  | | | |/ _` | '__/ _` |/ __/ _ \| '_ \ 
- * | |  | | |_| | (_| | | | (_| | (_| (_) | | | |
- * |_|  |_|\__, |\__,_|_|  \__,_|\___\___/|_| |_|
- *         __/ |                                
- *      |___/
+ *    _______                                _
+ *   |__   __|                              | |
+ *      | | ___  ___ ___  ___ _ __ __ _  ___| |_
+ *      | |/ _ \/ __/ __|/ _ \  __/ _` |/ __| __|
+ *      | |  __/\__ \__ \  __/ | | (_| | (__| |_
+ *      |_|\___||___/___/\___|_|  \__,_|\___|\__|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author Hydracon Team
- * 
+ * @author Tessetact Team
  * 
  *
 */
 
 namespace pocketmine\block;
 
+class WetSponge extends Solid {
 
-class WetSponge extends Solid{
+	protected $id = self::WET_SPONGE;
 
-	protected $id = self::WetSponge;
-
+	/**
+	 * WetSponge constructor.
+	 */
 	public function __construct(){
-
 	}
 
+	/**
+	 * @return int
+	 */
 	public function getResistance(){
 		return 3;
 	}
 
+	/**
+	 * @return float
+	 */
 	public function getHardness(){
 		return 0.6;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getName(){
 		return "Wet Sponge";
 	}
-
 }
