da410179214042c656a25095b2fc4d4f0149746e
Nekiechan <nekiechan678@gmail.com>
2017-07-13 09:49:10 -0500

    added support for NekoMine-MP.phar

--- start.ps1
+++ start.ps1
@@ -11,6 +11,8 @@ if(Test-Path "bin\php\php.exe"){
 
 if(Test-Path "PocketMine-MP.phar"){
 	$file = "PocketMine-MP.phar"
+}elseif(Test-Path "NekoMine-MP.phar"){
+	$file = "NekoMine-MP.phar"
 }elseif(Test-Path "src\pocketmine\PocketMine.php"){
 	$file = "src\pocketmine\PocketMine.php"
 }else{
