82c6ea9367205c1867180c24bcc22aa67b0a080f
Nekiechan <nekiechan678@gmail.com>
2017-07-13 13:04:35 -0500

    added some cool logo .3.

--- src/pocketmine/command/defaults/PlayerInfoCommand.php
+++ src/pocketmine/command/defaults/PlayerInfoCommand.php
@@ -1,4 +1,22 @@
 <?php
+/*
+*
+*  _   _      _         __  __ _                   __  __ _____  
+* | \ | |    | |       |  \/  (_)                 |  \/  |  __ \ 
+* |  \| | ___| | _____ | \  / |_ _ __   ___ ______| \  / | |__) |
+* | . ` |/ _ \ |/ / _ \| |\/| | | '_ \ / _ \______| |\/| |  ___/ 
+* | |\  |  __/   < (_) | |  | | | | | |  __/      | |  | | |     
+* |_| \_|\___|_|\_\___/|_|  |_|_|_| |_|\___|      |_|  |_|_|     
+*This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* @author NekoMine Team
+* @link http://www.nekomineforums.blogspot.com/
+* 
+*
+*/       
 declare(strict_types=1);
 namespace pocketmine\command\defaults;
 use pocketmine\command\CommandSender;
