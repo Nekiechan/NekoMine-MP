6a39cab5d11942577f046d32733e95d126ec0cc8
Nekiechan <nekiechan678@gmail.com>
2017-07-14 13:45:17 -0500

    Create ApiType.php

---
+++ src/pocketmine/API/ApiType.php
@@ -0,0 +1,33 @@
+<?php
+/*
+*
+*  _   _      _         __  __ _                   __  __ _____  
+* | \ | |    | |       |  \/  (_)                 |  \/  |  __ \ 
+* |  \| | ___| | _____ | \  / |_ _ __   ___ ______| \  / | |__) |
+* | . ` |/ _ \ |/ / _ \| |\/| | | '_ \ / _ \______| |\/| |  ___/ 
+* | |\  |  __/   < (_) | |  | | | | | |  __/      | |  | | |     
+* |_| \_|\___|_|\_\___/|_|  |_|_|_| |_|\___|      |_|  |_|_|     
+*This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* @author NekoMine Team
+* @link http://www.nekomineforums.blogspot.com/
+* 
+*
+*/      
+namespace pocketmine\API
+class Types{
+     protected type = null;
+     private PLAYER;
+     private SERVER;
+     private ENTITY;
+     private BLOCK;
+     private ERROR; //use api's to run code on error!
+     
+     final public function getType(){
+          return $this->type;
+     }
+     
+}
