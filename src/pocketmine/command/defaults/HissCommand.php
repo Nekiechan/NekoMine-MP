db814d0073fe37f7fa6bedca3d88b6beff5fbc95
Nekiechan <nekiechan678@gmail.com>
2017-07-18 07:30:47 -0500

    Create HissCommand.php

---
+++ src/pocketmine/command/defaults/HissCommand.php
@@ -0,0 +1,44 @@
+<?php
+/*
+*
+*  _   _      _         __  __ _                   __  __ _____  
+* | \ | |    | |       |  \/  (_)                 |  \/  |  __ \ 
+* |  \| | ___| | _____ | \  / |_ _ __   ___ ______| \  / | |__) |
+* | . ` |/ _ \ |/ / _ \| |\/| | | '_ \ / _ \______| |\/| |  ___/ 
+* | |\  |  __/   < (_) | |  | | | | | |  __/      | |  | | |     
+* |_| \_|\___|_|\_\___/|_|  |_|_|_| |_|\___|      |_|  |_|_|     
+*This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* @author NekoMine Team
+* @link http://www.nekomineforums.blogspot.com/
+* 
+*
+*/       
+declare(strict_types=1);
+namespace pocketmine\command\defaults;
+use pocketmine\command\CommandSender;
+use pocketmine\event\TranslationContainer;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+class HissCommand extends VanillaCommand{
+	public function __construct($name){
+		parent::__construct(
+			$name,
+      "§l§eAllows a player to hiss!",
+			"/hiss",
+      ["hisses"]
+		);
+		$this->setPermission("pocketmine.command.hiss");
+	}
+	public function execute(CommandSender $sender, $currentAlias, array $args){
+		if(!$this->testPermission($sender)){
+			return true;
+		}
+    $sender->getServer()->broadcastMessage("§a×§c" . $sender->getName() . " §aHisses!×");
+    return true;	
+	}
+ }
