adfa347bb51a03a54297de21e3d5d6166f14f384
Nekiechan <nekiechan678@gmail.com>
2017-07-22 13:37:07 -0500

    Update LoginPacket.php

--- src/pocketmine/network/mcpe/protocol/LoginPacket.php
+++ src/pocketmine/network/mcpe/protocol/LoginPacket.php
@@ -1,11 +1,11 @@
 <?php
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -14,37 +14,29 @@
  *
  * @author PocketMine Team
  * @link http://www.pocketmine.net/
- * 
+ *
  *
 */
-declare(strict_types=1);
 namespace pocketmine\network\mcpe\protocol;
 #include <rules/DataPacket.h>
 use pocketmine\network\mcpe\NetworkSession;
-use pocketmine\network\mcpe\protocol\DataPacket;
-class LoginPacket extends DataPacket {
+class LoginPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::LOGIN_PACKET;
-	const MOJANG_PUBKEY = "MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAE8ELkixyLcwlZryUQcu1TvPOmI2B7vX83ndnWRUaXm74wFfa5f/lwQNTfrLVHa2PmenpGI6JhIMUJaWZrjmMj90NoKNFSNBuKdm8rYiXsfaz3K36x/1U26HpG0ZxK/V1V";
 	const EDITION_POCKET = 0;
 	public $username;
 	public $protocol;
 	public $gameEdition;
 	public $clientUUID;
 	public $clientId;
-	public $clientData = [];
-	public $adRole;
-	public $currentInputMode;
-	public $defaultInputMode;
-	public $deviceModel;
-	public $deviceOS;
-	public $gameVersion;
-	public $guiScale;
-	public $tenantId;
-	public $uiProfile;
 	public $identityPublicKey;
 	public $serverAddress;
-	public $skinId = null;
-	public $skin = null;
+	public $skinId;
+	public $skin = "";
+	public $clientData = [];
+	
+	public $deviceModel;
+	public $deviceOS;
+
 	public function canBeSentBeforeLogin() : bool{
 		return true;
 	}
@@ -56,91 +48,44 @@ class LoginPacket extends DataPacket {
 		}
 		$this->gameEdition = $this->getByte();
 		$this->setBuffer($this->getString(), 0);
-		$time = time();
-		$chainData = json_decode($this->get($this->getLInt()))->{"chain"};
-		// Start with the trusted one
-		$chainKey = self::MOJANG_PUBKEY;
-		while(!empty($chainData)){
-			foreach($chainData as $index => $chain){
-				list($verified, $webtoken) = $this->decodeToken($chain, $chainKey);
-				if(isset($webtoken["extraData"])){
-					if(isset($webtoken["extraData"]["displayName"])){
-						$this->username = $webtoken["extraData"]["displayName"];
-					}
-					if(isset($webtoken["extraData"]["identity"])){
-						$this->clientUUID = $webtoken["extraData"]["identity"];
-					}
+		$chainData = json_decode($this->get($this->getLInt()));
+		foreach($chainData->{"chain"} as $chain){
+			$webtoken = $this->decodeToken($chain);
+			if(isset($webtoken["extraData"])){
+				if(isset($webtoken["extraData"]["displayName"])){
+					$this->username = $webtoken["extraData"]["displayName"];
 				}
-				if($verified){
-					$verified = isset($webtoken["nbf"]) && $webtoken["nbf"] <= $time && isset($webtoken["exp"]) && $webtoken["exp"] > $time;
+				if(isset($webtoken["extraData"]["identity"])){
+					$this->clientUUID = $webtoken["extraData"]["identity"];
 				}
-				if($verified and isset($webtoken["identityPublicKey"])){
-					// Looped key chain. #blamemojang
-					if($webtoken["identityPublicKey"] != self::MOJANG_PUBKEY) $chainKey = $webtoken["identityPublicKey"];
-					break;
-				}elseif($chainKey === null){
-					// We have already gave up
-					break;
+				if(isset($webtoken["identityPublicKey"])){
+					$this->identityPublicKey = $webtoken["identityPublicKey"];
 				}
 			}
-			if(!$verified && $chainKey !== null){
-				$chainKey = null;
-			}else{
-				unset($chainData[$index]);
-			}
 		}
-		list($verified, $skinToken) = $this->decodeToken($this->get($this->getLInt()), $chainKey);
-		if(isset($skinToken["AdRole"])){
-			$this->AdRole = $skinToken["AdRole"];
+		$this->clientData = $this->decodeToken($this->get($this->getLInt()));
+		$this->clientId = $this->clientData["ClientRandomId"] ?? null;
+		$this->serverAddress = $this->clientData["ServerAddress"] ?? null;
+		$this->skinId = $this->clientData["SkinId"] ?? null;
+		if(isset($this->clientData["SkinData"])){
+			$this->skin = base64_decode($this->clientData["SkinData"]);
 		}
-		if(isset($skinToken["ClientRandomId"])){
-			$this->clientId = $skinToken["ClientRandomId"];
+		if(isset($this->clientData["DeviceModel"])){
+			$this->deviceModel = $this->clientData["DeviceModel"];
 		}
-		if(isset($skinToken["CurrentInputMode"])){
-			$this->currentInputMode = $skinToken["CurrentInputMode"];
+		if(isset($this->clientData["DeviceOS"])){
+			$this->deviceOS = $this->clientData["DeviceOS"];
 		}
-		if(isset($skinToken["DefaultInputMode"])){
-			$this->defaultInputMode = $skinToken["DefaultInputMode"];
-		}
-		if(isset($skinToken["DeviceModel"])){
-			$this->deviceModel = $skinToken["DeviceModel"];
-		}
-		if(isset($skinToken["DeviceOS"])){
-			$this->deviceOS = $skinToken["DeviceOS"];
-		}
-		if(isset($skinToken["GameVersion"])){
-			$this->gameVersion = $skinToken["GameVersion"];
-		}
-		if(isset($skinToken["GuiScale"])){
-			$this->guiScale = $skinToken["GuiScale"];
-		}
-		if(isset($skinToken["ServerAddress"])){
-			$this->serverAddress = $skinToken["ServerAddress"];
-		}
-		if(isset($skinToken["SkinData"])){
-			$this->skin = base64_decode($skinToken["SkinData"]);
-		}
-		if(isset($skinToken["SkinId"])){
-			$this->skinId = $skinToken["SkinId"];
-		}
-		if(isset($skinToken["TenantId"])){
-			$this->TenantId = $skinToken["TenantId"];
-		}
-		if(isset($skinToken["UIProfile"])){
-			$this->UIProfile = $skinToken["UIProfile"];
-		}
-		if($verified){
-			$this->identityPublicKey = $chainKey;
-		}
-	}
-	public function decodeToken($token){
-		list($headB64, $payloadB64, $sigB64) = explode(".", $token);
-		return json_decode(base64_decode($payloadB64), true);
+
 	}
 	public function encode(){
+		//TODO
 	}
-	
-
+	public function decodeToken($token){
+		$tokens = explode(".", $token);
+		list($headB64, $payloadB64, $sigB64) = $tokens;
+		return json_decode(base64_decode($payloadB64), true);
+	}
 	public function handle(NetworkSession $session) : bool{
 		return $session->handleLogin($this);
 	}
