f77f3b345ec324e1b20698943290853708ccfa70
Nekiechan <nekiechan678@gmail.com>
2017-07-26 18:07:55 -0500

    Update EnderChest.php

--- src/pocketmine/tile/EnderChest.php
+++ src/pocketmine/tile/EnderChest.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____
@@ -18,56 +17,162 @@
  *
  *
 */
-
 namespace pocketmine\tile;
-
+use pocketmine\inventory\ChestInventory;
+use pocketmine\inventory\DoubleChestInventory;
+use pocketmine\inventory\InventoryHolder;
+use pocketmine\item\Item;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\nbt\NBT;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ListTag;
 use pocketmine\nbt\tag\StringTag;
-
-class EnderChest extends Spawnable implements Nameable {
-
-	/**
-	 * @return string
-	 */
-	public function getName() : string{
-		return isset($this->namedtag->CustomName) ? $this->namedtag->CustomName->getValue() : "Ender Chest";
+class EnderChest extends Spawnable implements InventoryHolder, Container, Nameable{
+	/** @var ChestInventory */
+	protected $inventory;
+	/** @var DoubleChestInventory */
+	protected $doubleInventory = null;
+	public function __construct(Level $level, CompoundTag $nbt){
+		parent::__construct($level, $nbt);
+		$this->inventory = new EnderChestInventory($this);
+		if(!isset($this->namedtag->Items) or !($this->namedtag->Items instanceof ListTag)){
+			$this->namedtag->Items = new ListTag("Items", []);
+			$this->namedtag->Items->setTagType(NBT::TAG_Compound);
+		}
+		for($i = 0; $i < $this->getSize(); ++$i){
+			$this->inventory->setItem($i, $this->getItem($i));
+		}
 	}
-
+	public function close(){
+		if($this->closed === false){
+			foreach($this->getInventory()->getViewers() as $player){
+				$player->removeWindow($this->getInventory());
+			}
+			foreach($this->getInventory()->getViewers() as $player){
+				$player->removeWindow($this->getRealInventory());
+			}
+			$this->inventory = null;
+			parent::close();
+		}
+	}
+	public function saveNBT(){
+		$this->namedtag->Items = new ListTag("Items", []);
+		$this->namedtag->Items->setTagType(NBT::TAG_Compound);
+		for($index = 0; $index < $this->getSize(); ++$index){
+			$this->setItem($index, $this->inventory->getItem($index));
+		}
+	}
 	/**
+	 * @return int
+	 */
+	public function getSize(){
+		return 27;
+	}
+	/**
+	 * @param $index
+	 *
+	 * @return int
+	 */
+	protected function getSlotIndex($index){
+		foreach($this->namedtag->Items as $i => $slot){
+			if((int) $slot["Slot"] === (int) $index){
+				return (int) $i;
+			}
+		}
+		return -1;
+	}
+	/**
+	 * This method should not be used by plugins, use the Inventory
+	 *
+	 * @param int $index
+	 *
+	 * @return Item
+	 */
+	public function getItem($index){
+		$i = $this->getSlotIndex($index);
+		if($i < 0){
+			return Item::get(Item::AIR, 0, 0);
+		}else{
+			return Item::nbtDeserialize($this->namedtag->Items[$i]);
+		}
+	}
+	/**
+	 * This method should not be used by plugins, use the Inventory
+	 *
+	 * @param int  $index
+	 * @param Item $item
+	 *
 	 * @return bool
 	 */
+	public function setItem($index, Item $item){
+		$i = $this->getSlotIndex($index);
+		$d = $item->nbtSerialize($index);
+		if($item->getId() === Item::AIR or $item->getCount() <= 0){
+			if($i >= 0){
+				unset($this->namedtag->Items[$i]);
+			}
+		}elseif($i < 0){
+			for($i = 0; $i <= $this->getSize(); ++$i){
+				if(!isset($this->namedtag->Items[$i])){
+					break;
+				}
+			}
+			$this->namedtag->Items[$i] = $d;
+		}else{
+			$this->namedtag->Items[$i] = $d;
+		}
+		return true;
+	}
+	/**
+	 * @return ChestInventory
+	 */
+	public function getInventory(){
+		return $this->inventory;
+	}
+	/**
+	 * @return ChestInventory
+	 */
+	public function getRealInventory(){
+		return $this->inventory;
+	}
+	
+	public function getName(){
+		return isset($this->namedtag->CustomName) ? $this->namedtag->CustomName->getValue() : "ENDER_CHEST";
+	}
 	public function hasName(){
 		return isset($this->namedtag->CustomName);
 	}
-
-	/**
-	 * @param void $str
-	 */
 	public function setName($str){
 		if($str === ""){
 			unset($this->namedtag->CustomName);
 			return;
 		}
-
 		$this->namedtag->CustomName = new StringTag("CustomName", $str);
 	}
-
-	/**
-	 * @return CompoundTag
-	 */
+	
+	
+	
 	public function getSpawnCompound(){
-		$c = new CompoundTag("", [
-			new StringTag("id", Tile::ENDER_CHEST),
-			new IntTag("x", (int) $this->x),
-			new IntTag("y", (int) $this->y),
-			new IntTag("z", (int) $this->z)
-		]);
-
+		if($this->isPaired()){
+			$c = new CompoundTag("", [
+				new StringTag("id", Tile::ENDER_CHEST),
+				new IntTag("x", (int) $this->x),
+				new IntTag("y", (int) $this->y),
+				new IntTag("z", (int) $this->z)
+			]);
+		}else{
+			$c = new CompoundTag("", [
+				new StringTag("id", Tile::ENDER_CHEST),
+				new IntTag("x", (int) $this->x),
+				new IntTag("y", (int) $this->y),
+				new IntTag("z", (int) $this->z)
+			]);
+		}
 		if($this->hasName()){
 			$c->CustomName = $this->namedtag->CustomName;
 		}
 		return $c;
 	}
-
 }
