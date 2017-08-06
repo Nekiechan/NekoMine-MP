a5c49b393f4c330a8498fc09462b03c043ce9549
Nekiechan <nekiechan678@gmail.com>
2017-08-06 10:14:24 -0500

    fixed limited OP

--- src/pocketmine/command/defaults/OpCommand.php
+++ src/pocketmine/command/defaults/OpCommand.php
@@ -50,16 +50,18 @@ class OpCommand extends VanillaCommand{
 
 			return false;
 		}
+		if($player instanceof Player){
         if($sender->getServer()->getNekoMineConfigValue("limited-op", false)){
             $sender->sendMessage(TextFormat::RED . "[NOTICE] Op is disabled ingame! Please run this in the Console!");
             return true;
+        }
         }
 		$name = array_shift($args);
 
 		$player = $sender->getServer()->getOfflinePlayer($name);
 		Command::broadcastCommandMessage($sender, new TranslationContainer("commands.op.success", [$player->getName()]));
 		if($player instanceof Player){
-			$player->sendMessage(TextFormat::GRAY . "You are now op!");
+			$player->sendMessage(TextFormat::GREEN . "[NOTICE] " . TextFormat::GRAY . "You are now op!");
 		}
 		$player->setOp(true);
 		return true;
