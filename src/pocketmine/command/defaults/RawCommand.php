7ef5b0fa06f8b4bddb06334cbb101f40f1b55faf
Nekiechan <nekiechan678@gmail.com>
2017-08-08 14:51:20 -0500

    Update RawCommand.php

--- src/pocketmine/command/defaults/RawCommand.php
+++ src/pocketmine/command/defaults/RawCommand.php
@@ -49,7 +49,7 @@ class RawCommand extends VanillaCommand{
       $sender->sendMessage("§l§cPut a Msg! §a/raw <msg>");
       return false;
     }
-		$sender->getServer()->broadcastMessage(TextFormat::RED . "[NOTICE] " . implode(" ", $args));
+		$sender->getServer()->broadcastMessage(implode(" ", $args));
 		return true;
     }
 }
